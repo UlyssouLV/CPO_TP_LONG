3136f182580711219049dd295ea76c1b027d4d85
Ulysse Long <ulysse.long@epfedu.fr>
2023-09-18 13:03:29 +0200

    Exercice 1

---
+++ Exo1/src/main/java/com/ulysselongepf/exo1/Exo1.java
@@ -0,0 +1,23 @@
+/**
+ * TP0 
+ * Calculatrice
+ * @author Ody
+ */
+
+package com.ulysselongepf.exo1;
+
+import java.util.Scanner;
+
+
+public class Exo1 {
+
+    public static void main(String[] args) {
+        
+        String prenom;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Comment vous appelez-vous ?");
+        prenom = sc.nextLine();
+
+    }
+}
