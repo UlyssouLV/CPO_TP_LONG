51557cd31ece5b57974547b96ef0cf12e40ff3d0
Ulysse Long <ulysse.long@epfedu.fr>
2023-09-26 11:48:02 +0200

    TP1

--- TP1_manipNombresInt_LONG/src/main/java/com/ulysselongepf/tp1_manipnombresint_long/TP1_manipNombresInt_LONG.java
+++ TP1_manipNombresInt_LONG/src/main/java/com/ulysselongepf/tp1_manipnombresint_long/TP1_manipNombresInt_LONG.java
@@ -1,17 +1,30 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+26 Septembre 2023
+TP1
+Ulysse LONG
  */
 
 package com.ulysselongepf.tp1_manipnombresint_long;
 
-/**
- *
- * @author Ody
- */
+import java.util.Scanner;
+
+
 public class TP1_manipNombresInt_LONG {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        // Variables éventuelles
+        int entier1;
+        int entier2;
+        
+        //Demande de saisie et affectation
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Saisissez deux entiers :");
+        entier1 = sc.nextInt();
+        entier2 = sc.nextInt();
+        
+        //Affichage saisie + différence + produit + Quotient + Reste division euclidienne
+        System.out.print("Entier 1 : "+entier1 +"\nEntier 2 : "+ entier2 +"\nDifférence : "+ (entier1-entier2)+"\nProduit : "+ entier1*entier2 +"\nQuotient : "+ (entier1/entier2) + "\nReste : "+(entier1%entier2));
     }
 }
