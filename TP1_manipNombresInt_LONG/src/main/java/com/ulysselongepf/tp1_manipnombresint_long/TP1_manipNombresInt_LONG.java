260d8a35469aa1f4ed9b84281ce1feeb660425dd
Ulysse Long <ulysse.long@epfedu.fr>
2023-09-26 11:36:22 +0200

    TP1

---
+++ TP1_manipNombresInt_LONG/src/main/java/com/ulysselongepf/tp1_manipnombresint_long/TP1_manipNombresInt_LONG.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.ulysselongepf.tp1_manipnombresint_long;
+
+/**
+ *
+ * @author Ody
+ */
+public class TP1_manipNombresInt_LONG {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
