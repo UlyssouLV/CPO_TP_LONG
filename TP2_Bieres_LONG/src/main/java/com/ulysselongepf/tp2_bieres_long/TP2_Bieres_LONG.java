e026b4db8985dd6f8708d5b6e90aa0068bb71cec
Ulysse Long <ulysse.long@epfedu.fr>
2023-09-27 22:08:07 +0200

    TP2 : Création de premières classes et objets

---
+++ TP2_Bieres_LONG/src/main/java/com/ulysselongepf/tp2_bieres_long/TP2_Bieres_LONG.java
@@ -0,0 +1,15 @@
+/*
+Ulysse LONG 
+27 Septembre
+TP2
+*/
+
+package com.ulysselongepf.tp2_bieres_long;
+
+
+public class TP2_Bieres_LONG {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
