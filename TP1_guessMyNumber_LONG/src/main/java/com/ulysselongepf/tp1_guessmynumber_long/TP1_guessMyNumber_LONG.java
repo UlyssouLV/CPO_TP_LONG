f2105f84783d6204417498677596b7e65206e807
Ulysse Long <ulysse.long@epfedu.fr>
2023-09-27 15:01:20 +0200

    TP1 Partie 3 finie

--- TP1_guessMyNumber_LONG/src/main/java/com/ulysselongepf/tp1_guessmynumber_long/TP1_guessMyNumber_LONG.java
+++ TP1_guessMyNumber_LONG/src/main/java/com/ulysselongepf/tp1_guessmynumber_long/TP1_guessMyNumber_LONG.java
@@ -14,9 +14,10 @@ public class TP1_guessMyNumber_LONG {
     public static void main(String[] args) {
         
         //Variables éventuelles
-        int nb;
+        int nb, difficulty, diff;
         int essai=1;
         boolean condition = true;
+        
         
         // Génération nombre aléatoire dans n
         Random generateurAleat = new Random();
@@ -26,20 +27,134 @@ public class TP1_guessMyNumber_LONG {
          // Affichage Saisie et Input
         Scanner sc;
         sc = new Scanner(System.in);
-        System.out.println("\n Please enter a number :"); 
-        nb = sc.nextInt(); // Saisie nombre
+        System.out.println("Choisissez le niveau de difficulté :\n1) Facile\n2) Normal\n3) Difficile");
+        difficulty = sc.nextInt();
+       
+        //Exception difficulté
+        while(true){
+            switch(difficulty){
+                case 1 -> System.out.println("Difficulté facile choisie");
+                case 2 -> System.out.println("Difficulté normale choisie");
+                case 3 -> System.out.println("Difficulté difficile choisie");
+                default -> {
+                    System.out.println("Erreur de saisie, choisissez parmi 1/2/3");
+                }
+            } // fin du switch
+            break;
+        }//fin du while
         
+        //Jeu
         do {
+            //Saisie nombre utilisateur
+            System.out.println("\n Please enter a number :"); 
+            nb = sc.nextInt(); // Saisie nombre
+            diff = n-nb; // Différence entre n et nb 
+            
+            //Gagné
             if(nb==n){
                 System.out.println("Gagné en "+essai+" tentatives");
                 condition=false;
-            }else if(nb>n){
-                System.out.println("Trop grand");
-                essai++;
-            } else {
-                System.out.println("Trop petit");
-                essai++;
+            } 
+            
+            // si n < nb : diff <0 => trop Grand
+            else if(n<nb){
+                diff=-diff;
+
+                switch (difficulty) {
+                    // difficulté facile          
+                    case 1 -> {
+                        //Distance > 40
+                        if (diff>40){
+                            System.out.println("Vraiment trop grand");
+                            essai++;
+                        } 
+                        // Distance <40 et >20
+                        else if ( diff <=40 && (diff)>=10){
+                            System.out.println("Trop grand");
+                            essai++;
+                        } 
+                        // Distance < 10
+                        else if (diff < 10 ){
+                            System.out.println("Un tout petit peu trop grand");
+                            essai++;
+                        }
+                        //On incrémente de 1 le nombre d'essai
+                        essai++;
+                    }
+                    
+                    // difficulté Normale 
+                    case 2 -> {
+                       System.out.println("Trop grand");
+                       essai++;
+                    }
+                    
+                    // difficulté Difficile
+                    case 3 -> {
+                        
+                        System.out.println("Trop grand");
+                        
+                        //Moins de 10 tentatives
+                        if(essai<10){
+                        System.out.println("Il vous reste "+(10-essai)+" tentatives");
+                        essai++;
+                        } else {
+                            System.out.println("Perdu");
+                            condition=false;
+                        }
+                    }
+                    default -> {
+                    }
+                }// Fin du switch 
+                }// Fin trop grnad
+            
+                // si n > nb : diff > 0 => Trop petit
+                else if (n>nb){
+               
+                switch (difficulty) {
+                    // difficulté facile
+                    case 1 -> {
+                        //Distance > 40
+                        if (diff>40){
+                            System.out.println("Vraiment trop petit");
+                            essai++;
+                        } 
+                        // Distance <40 et > 20
+                        else if ( diff <=40 && diff>=10){
+                            System.out.println("Trop petit");
+                            essai++;
+                        } 
+                        // Distance < 10
+                        else if (diff < 10 ){
+                            System.out.println("Un tout petit peu trop petit");
+                            essai++;
+                        }
+                    }
+                    
+                    // Difficulté Normale
+                    case 2 -> {
+                       System.out.println("Trop petit");
+                       essai++;
+                    }
+                    // Difficulté Difficile
+                    case 3 ->  {
+                        
+                        System.out.println("Trop petit");
+                        //Moins de 10 tentatives
+                        if(essai<10){
+                        System.out.println("Il vous reste "+(10-essai)+" tentatives");
+                        essai++;
+                        } else {
+                            System.out.println("Perdu");
+                            condition=false;
+                        }
+                    }
+                    default -> {
+                    }
+                } // Fin du switch
+
             }
         }while(condition);
-    }
-}
+        
+    }//Fin Main
+}//Fin Programme
+
