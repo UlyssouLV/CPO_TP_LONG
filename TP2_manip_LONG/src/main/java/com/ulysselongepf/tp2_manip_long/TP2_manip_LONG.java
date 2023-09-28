c8ba010e22fdb0e4c827bf3ca32b234055af331b
Ulysse Long <ulysse.long@epfedu.fr>
2023-09-28 02:07:52 +0200

    TP2 : Manipulation des objets par leur référence

---
+++ TP2_manip_LONG/src/main/java/com/ulysselongepf/tp2_manip_long/TP2_manip_LONG.java
@@ -0,0 +1,14 @@
+/*
+27 Septembre 2023
+Ulysse LONG
+TP2 : Manipulation des objets
+*/
+
+package com.ulysselongepf.tp2_manip_long;
+
+public class TP2_manip_LONG {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
