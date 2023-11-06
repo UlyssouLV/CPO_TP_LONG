9700c06d203fa0cf754b876c80910b3025ab6630
Ulysse Long <ulysse.long@epfedu.fr>
2023-11-06 15:31:55 +0100

    La classe GrilleDeCellules a été ajoutée et est fonctionnelle

--- LightOff_LONG_version_console/src/main/java/com/ulysselongepf/lightoff_long_version_console/LightOff_LONG_version_console.java
+++ LightOff_LONG_version_console/src/main/java/com/ulysselongepf/lightoff_long_version_console/LightOff_LONG_version_console.java
@@ -5,19 +5,11 @@ public class LightOff_LONG_version_console {
 
     public static void main(String[] args) {
         
-        CelluleLumineuse Cellule1 = new CelluleLumineuse();
-        System.out.println(Cellule1);
-        System.out.println(Cellule1.estEteint());
+      GrilleDeCellules Grille1 = new GrilleDeCellules(6,6);
+      Grille1.melangerMatriceAleatoirement(5);
+      System.out.println(Grille1);
+ 
 
-        Cellule1.activerCellule();
-        System.out.println(Cellule1);
-        System.out.println(Cellule1.estEteint());
-        
-        Cellule1.eteindreCellule();
-        System.out.println(Cellule1);
-        System.out.println(Cellule1.estEteint());
-        
-        System.out.println(Cellule1.getEtat());
 
     }
 }
